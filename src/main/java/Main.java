efabb15d6e3bce7131eaf7eb35c9e5129a2ac813
NAleshina <minerva987@gmail.com>
2020-07-18 19:51:51 +0300

    Added application

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.security.Provider;
+
+public class Main {
+  public static void main(String[] args) {
+    BonusService service = new BonusService();
+
+    long amount = 1000_60;
+    boolean registered = true;
+
+    service.calculate(amount, registered);
+  }
+
+}
+
+
